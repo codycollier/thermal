f0f0a8093634a415abcb2bade79748dbc77f54d7
Cody Collier <cody@telnet.org>
2014-04-30 21:31:30 -0500

    working on exercise util

--- exercise.go
+++ exercise.go
@@ -11,6 +11,6 @@ func main() {
 	start := time.Now()
 	fmt.Printf("start: %s\n", start)
 	s := new(thermal.Switch)
-	fmt.Printf("Switch instance created: %s\n", s)
+	fmt.Printf("Switch instance created: %s\n", *s)
 	fmt.Printf("stop: %s\n", time.Now())
 }
