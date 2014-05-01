8ad24d32ab79cf698ce4af1a47ba0863edbda204
Cody Collier <cody@telnet.org>
2014-04-30 21:24:45 -0500

    setting up skeleton structures

---
+++ exercise.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"github.com/codycollier/thermal/thermal"
+	"time"
+)
+
+// exercise will exercise and test the thermal library
+func main() {
+	start := time.Now()
+	fmt.Printf("start: %s\n", start)
+	s := new(thermal.Switch)
+	fmt.Printf("Switch instance created: %s\n", s)
+	fmt.Printf("stop: %s\n", time.Now())
+}
---
+++ thermal/switch.go
@@ -0,0 +1,4 @@
+package thermal
+
+// Swith provides an api to create, manage, and use a Telehash switch instance
+type Switch struct{}
