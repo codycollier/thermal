82325995b641f142027845fdacf064f66ba15e83
Cody Collier <cody@telnet.org>
2014-05-11 09:35:39 -0500

    working on packet encoding/decoding

--- packet.go
+++ packet.go
@@ -1,20 +1,29 @@
 package thermal
 
-// decodedPacket holds the pieces of a decoded telehash packet
+import (
+	"encoding/json"
+)
+
+// A decodedPacket holds the pieces of a decoded telehash packet
 // packet encoding:
 //  <length-of-head>[HEAD][BODY]
 type decodedPacket struct {
 	headLength int
 	head       []byte
-	json       string
 	bodyLength int
 	body       []byte
+	json       string
 }
+
+// Should the json decoding be handled here as well?  Or in a channel/type
+// specific area?  If here, create a map of json/channel types and their
+// structs?  It might be good to have all the json schema here in the same
+// place.
 
 // decodePacket accepts a telehash packet and breaks it in to components
 func decodePacket(packet []byte) (decodedPacket, error) {
 }
 
-// encodePacket accepts head and body payloads and encodes them to a packet
-func encodePacket(json string, body []byte) (packet []byte, err error) {
+// encodePacket accepts json and body payloads and encodes them to a packet
+func encodePacket(json string, body []byte) ([]byte, error) {
 }
