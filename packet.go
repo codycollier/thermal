2242b524f5df88b99c59157f44ab210262071fcf
Cody Collier <cody@telnet.org>
2014-05-11 18:58:47 -0500

    early work on packet encoding and decoding

--- packet.go
+++ packet.go
@@ -1,7 +1,9 @@
 package thermal
 
 import (
-	"encoding/json"
+	"bytes"
+	"encoding/binary"
+	"fmt"
 )
 
 // A decodedPacket holds the pieces of a decoded telehash packet
@@ -15,15 +17,83 @@ type decodedPacket struct {
 	json       string
 }
 
-// Should the json decoding be handled here as well?  Or in a channel/type
-// specific area?  If here, create a map of json/channel types and their
-// structs?  It might be good to have all the json schema here in the same
-// place.
-
 // decodePacket accepts a telehash packet and breaks it in to components
-func decodePacket(packet []byte) (decodedPacket, error) {
+func decodePacket(packetBytes []byte) (decodedPacket, error) {
+
+	var packet decodedPacket
+	var headLength int
+	var head []byte
+	var bodyLength int
+	var body []byte
+	var json string
+	var err error = nil
+
+	if len(packetBytes) < 3 {
+		packet = decodedPacket{}
+		err = fmt.Errorf("Packet must be at least three packetBytes long")
+		return packet, err
+	}
+
+	firstTwo := bytes.NewReader(packetBytes[:2])
+	err = binary.Read(firstTwo, binary.BigEndian, &headLength)
+	if err == nil {
+		packet = decodedPacket{}
+		return packet, fmt.Errorf("Packet head length is not a valid integer (err: %s)", err)
+	}
+
+	bodyLength = len(packetBytes) - headLength - 2
+
+	if headLength == 0 {
+		head = nil
+		json = ""
+	} else {
+		head = packetBytes[2 : 2+headLength]
+		json = string(head)
+	}
+
+	if bodyLength == 0 {
+		body = nil
+	} else {
+		body = packetBytes[2+headLength:]
+	}
+
+	packet = decodedPacket{
+		headLength: headLength,
+		bodyLength: bodyLength,
+		head:       head,
+		body:       body,
+		json:       json,
+	}
+
+	return packet, nil
 }
 
 // encodePacket accepts json and body payloads and encodes them to a packet
 func encodePacket(json string, body []byte) ([]byte, error) {
+
+	var packet, headLength, head []byte
+	var err error
+	packet = make([]byte, 0)
+
+	if json != "" {
+		head = []byte(json)
+	} else {
+		head = nil
+	}
+
+	// The head-length will be the first two bytes of the packet (network byte order / big endian)
+	headBytes := new(bytes.Buffer)
+	err = binary.Write(headBytes, binary.BigEndian, len(head))
+	if err == nil {
+		return nil, fmt.Errorf("Error writing head length to packet (err: %s", err)
+	}
+	headLength = []byte(headBytes.Bytes())
+
+	// Assemble the packet
+	packet = append(packet, headLength...)
+	packet = append(packet, head...)
+	packet = append(packet, body...)
+
+	return packet, err
+
 }
